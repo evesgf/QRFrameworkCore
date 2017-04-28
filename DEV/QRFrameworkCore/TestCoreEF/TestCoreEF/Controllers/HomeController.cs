ba67d83f80b22a9d6dc90ff6961ac18233befb16
evesgf <evesgf@163.com>
2017-04-28 18:14:03 +0800

    版本重提交

---
+++ DEV/QRFrameworkCore/TestCoreEF/TestCoreEF/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using TestCoreEF.Dao;
+using TestCoreEF.Models;
+
+namespace TestCoreEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TestDBContext _testDBContext;
+        private readonly ApplicationConfiguration _options;
+
+        public HomeController(TestDBContext testDBContext, IOptions<ApplicationConfiguration> appConfiguration)
+        {
+            _testDBContext = testDBContext;
+            _options = appConfiguration.Value;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new SYS_Model
+            {
+                UserName = "123"
+            };
+            _testDBContext.Add(model);
+            _testDBContext.SaveChanges();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = _testDBContext.Sys_Model.ToList();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            //读取配置
+
+
+            ViewData["Message"] = "FileUpPath:"+ _options.FileUpPath;
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
